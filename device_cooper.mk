0e74ea736b3d21483db810262df7b05e5e63ec0c
erikcas <erikcas1972@gmail.com>
2013-04-01 21:00:18 +0200

    update

--- device_cooper.mk
+++ device_cooper.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 ## Inherit products
 $(call inherit-product, device/samsung/msm7x27-common/common.mk)
 $(call inherit-product, vendor/samsung/cooper/vendor_blobs.mk)
