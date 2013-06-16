760ab58308133c3db9ecd5cf77c98b4e406207ef
erikcas <erikcas1972@gmail.com>
2013-06-16 23:30:23 +0200

    adjustments

--- cm.mk
+++ cm.mk
@@ -1,20 +1,19 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# BootAnimation
+TARGET_BOOTANIMATION_NAME := vertical-320x480
 
-# Inherit some common CM stuff.
+# Inherit device configuration for Cooper
+$(call inherit-product, device/samsung/cooper/full_cooper.mk)
+
+# Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/cooper/full_cooper.mk)
+# Include GSM stuff
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := GalaxyCooper
 PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5830
--include vendor/cm/config/common_versions.mk
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cooper
 PRODUCT_NAME := cm_cooper
-
-## Bootanimation
-TARGET_BOOTANIMATION_NAME := vertical-320x480
